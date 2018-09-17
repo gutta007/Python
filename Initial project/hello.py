02375dfac1e5386d961fce7077796f2d1f1bf4fd
rahul <rahul@DESKTOP-MTA6BTV>
2018-09-16 19:20:30 -0700

    updated the code

--- Initial project/hello.py
+++ Initial project/hello.py
@@ -13,3 +13,18 @@ if a==10:
     print("a is 10")
 else:
     print("a is not equal to 10")
+    
+b = 201
+print("b is :" + str(b))
+
+if a==b:
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+    
+ if a>b:
+     print("a is greater to b")
+elif b>a:
+    print("b is greater to a")   
+else:
+    ("print a and b are equal")
